98148cf86af41f135f2789428d777bf3ba33f592
Zhigen Zhao <zhaozhg@guoliang.guoliang>
2022-07-07 13:56:06 -0400

    update example.R

--- example/example.R
+++ example/example.R
@@ -6,6 +6,8 @@ m <- n/H
 sigma <- 1
 
 p <- 20
+q <- 5
+
 
 set.seed(2)
 D <- 1
@@ -14,12 +16,13 @@ D <- 1
 beta <- array(0, c(p, D) )
 beta <- rnorm(p, 0, 1)
 
+gamma <- rnorm(q, 0, 1)
 
 X <- matrix( rnorm(p*n), c(n, p) )
+Z <- matrix( rnorm(q*n), c(n, q) )
 
 
-Z <- X%*% beta
-Y <-  sin(Z[,1])*exp(Z[,1])*0.2 + sigma*rnorm(n,0,1)
+Y <-  sin( X%*%beta*0.2)*exp( X%*% beta *0.2 ) + sigma*rnorm(n,0,1)
 
 
 res <- gSNR( X, Y, discrete=FALSE, pvalue=TRUE)
@@ -35,3 +38,8 @@ Y2[ ORD[ (floor(2*n/3)+1):n] ] <- 3
 res.2 <- gSNR( X, Y2, discrete=TRUE, pvalue=TRUE)
 res.2$stat
 res.2$pvalue
+
+
+Y <- sin( Z%*%gamma ) + sin( X %*%beta*0.2 )*exp( X %*% beta * 0.2 ) + sigma*rnorm(n,0,1)
+## res.covariate <- gSNR:::gSNR_Covariate( X, Z, Y, discrete=FALSE, pvalue=TRUE)
+res.covariate <- gSNR:::gSNR_Covariate( X, Z, Y, discrete=FALSE, pvalue=TRUE)
